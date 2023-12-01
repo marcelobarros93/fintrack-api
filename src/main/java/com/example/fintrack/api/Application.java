d3500ced373f6d6b5899e0bff1d21a9f9b283fba
Marcelo Barros <marcelobarros-sms@hotmail.com>
2023-12-01 08:40:37 -0300

    remove unused configuration

--- src/main/java/com/example/fintrack/api/Application.java
+++ src/main/java/com/example/fintrack/api/Application.java
@@ -18,15 +18,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("GET", "POST", "PUT", "DELETE");
-			}
-		};
-	}
 }
